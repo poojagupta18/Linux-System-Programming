1c68f293e5069062e61f02c067b63ac8b90ad46d
poojagupta18 <pujagupta5377@gmail.com>
2020-09-19 15:56:31 +0530

    Print Largest file name

--- Assignment-3/Assignment3_3/Assignment3_3.c
+++ Assignment-3/Assignment3_3/Assignment3_3.c
@@ -13,7 +13,10 @@ int main(int argc, char* argv[])
     struct stat buf;
     int count = 0;
     long *blockSize = NULL;
-    int i = 0;
+    int i,j = 0;
+    long max = 0;
+    int maxValueindex = -1;
+
 
     if(argc != 2)
     {
@@ -23,30 +26,68 @@ int main(int argc, char* argv[])
 
     d = opendir(argv[1]);
 
-	printf("Open dir = %p", d);
-     if (d)
-    {
-        while ((dir = readdir(d)) != NULL)
-        {
-            count++;      
-        }
-        closedir(d);
-    }
- 	blockSize = (long*) malloc(count * sizeof(long));
- 
-    d = opendir(argv[1]);
+    printf("Open dir = %p", d);
     if (d)
     {
         while ((dir = readdir(d)) != NULL)
         {
-           
-            fd = stat(dir->d_name, &buf);  
-	    blockSize[i] = buf.st_size;
- 	    printf("%s and size = %ld\n", dir->d_name, buf.st_size);        
+            count++;
         }
         closedir(d);
     }
 
+    blockSize = (long*) malloc(count * sizeof(long));
 
+
+    d = opendir(argv[1]);
+
+    if (d)
+    {
+        while ((dir = readdir(d)) != NULL)
+        {
+
+            fd = stat(dir->d_name, &buf);
+
+            blockSize[i] = buf.st_size;
+
+          //  printf("\n%s and size = %ld\n", dir->d_name, buf.st_size);
+        }
+        closedir(d);
+    }
+
+   /* for(i = 0; i < count; i++)
+    {
+        printf("Blcksize at index %d is %ld\n", i, blockSize[i]);
+    }
+   */
+
+    for(i = 0; i < count; i++)
+    {
+        if(blockSize[i] > max )
+        {
+            max = blockSize[i];
+            maxValueindex = i;
+        }
+    }
+
+    //printf(" maxValueindex = %d  and max value is %ld\n",maxValueindex, max );
+
+    d = opendir(argv[1]);
+
+    if (d)
+    {
+        while ((dir = readdir(d)) != NULL )
+        {
+
+            fd = stat(dir->d_name, &buf);
+            if(j == maxValueindex)
+            {
+                printf("\nLargest file name is %s and size = %ld\n", dir->d_name, buf.st_size);
+            }
+            j++;
+
+        }
+        closedir(d);
+    }
     return 0;
 }
