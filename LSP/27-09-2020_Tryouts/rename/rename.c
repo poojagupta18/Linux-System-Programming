60a539097de8877469c1769e0de18703bda1a07e
poojagupta18 <pujagupta5377@gmail.com>
2020-09-27 21:52:00 +0530

    rename file tryouts

---
+++ LSP/27-09-2020_Tryouts/rename/rename.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(int argc, char* argv[])
+{
+    int ret = 0;
+    
+    //oldfile, newfile
+    ret = rename("data1.txt", "RenamedData.txt");
+    
+    if(ret == -1)
+    {
+        printf("Rename failed %d", ret);
+    }
+    printf("Rename successful with ret %d", ret);
+
+    return 0;
+}
+
