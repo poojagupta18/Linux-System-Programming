1671ac021af192e47b5661d3985a61c83d291945
poojagupta18 <pujagupta5377@gmail.com>
2020-09-27 21:51:04 +0530

    IoRedirections tryouts

---
+++ LSP/27-09-2020_Tryouts/IoRedirection/Demo.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int no1 = 0;
+    int no2 = 0;
+    int no3 = 0;
+    int ans = 0;
+
+    printf("Enter 3 numbers:\n");
+
+    scanf("%d %d %d", &no1, &no2, &no3);
+
+    ans = no1 + no2 + no3;
+
+    printf("Addition is %d\n", ans);
+    return 0;
+}
+
