879527842adfd4efa7a6165cd2ebeeadbfa4604c
poojagupta18 <pujagupta5377@gmail.com>
2020-10-17 02:25:14 +0530

    Print home directory and login user

---
+++ Assignment-8/Assignment-7/Assignment-7.c
@@ -0,0 +1,19 @@
+//Write a program login user name and home directory.
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <pwd.h>
+
+int main(int argc, char* argv[], char *env[])
+{
+    struct passwd *pw = getpwuid(getuid());
+
+    const char *homedir = pw->pw_dir;
+
+    printf("Home dir name is %s\n",pw->pw_dir);
+    printf("Login user name is %d\n", getuid());
+
+    return 0;
+}
+
